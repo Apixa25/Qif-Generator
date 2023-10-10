a41ffa2c6296ca8dc5f1ff4ec0bec57c27424a2c
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-10-10 12:51:37 -0500

    added quotesAPI

---
+++ assets/js/script.js
@@ -0,0 +1,130 @@
+// confirming that our js file is properly attached
+console.log("js file loading");
+
+// ~ \/ below is creating a variable in vue \/ ~ //
+// const app = Vue.createApp({
+//     template: '<h2>Hello CDN Vue</h2>'
+// });
+
+// ~ \/ below is how we add a script to an ID in the HTML \/ ~ //
+// app.mount('#app');
+
+// const savedWords = Vue.createApp({
+//     template: '<h2>This is where saved words can go</h2>'
+// });
+
+// savedWords.mount('#saved-words');
+
+// const generatedQuote = Vue.createApp({
+//     template: '<h2>This is where our generated quote can go</h2>'
+// });
+
+// generatedQuote.mount('#generated-quote');
+
+// const generatedGiphy = Vue.createApp({
+//     template: '<h2>This is where our generated GIPHY can go</h2>'
+// });
+
+// generatedGiphy.mount('#generated-giphy');
+
+// const addWord = Vue.createApp({
+//     method: {
+//         handleAddWord() {
+//         console.log("add word")
+//         } 
+//     }
+// })
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~ VANILLA JAVASCRIPT ~~~~~~~~~~~~~~ //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+// ~~~~~~~~~~ DOCUMENT SELECTORS ~~~~~~~~~~ //
+var wordInputForm = document.getElementById("word-input-form");
+var wordInput = document.getElementById("word-input");
+var inputButton = document.getElementById("input-button");
+var clearWordsButton = document.getElementById("clear-words");
+var savedWords = document.getElementById("saved-words");
+var generatedQuote = document.getElementById("generated-quote");
+var generatedGiphy = document.getElementById("generated-giphy");
+var loggedWords = [];
+
+// ~~~~~~~~~~ FUNCTIONS & EVENT LISTENERS ~~~~~~~~~~ //
+function submitWord(e) {
+    e.preventDefault();
+    var wordValue = wordInput.value;
+    fetchQuote(wordValue);
+    fetchGiphy(wordValue);
+}
+
+function fetchQuote(wordValue) {
+    var wordValue = "happy";
+    var quoteURL = `https://zenquotes.io/api/quotes/[20f0ab3d282c49b93e06cf1487491f0d]&keyword=${wordValue}`;
+    fetch(quoteURL)
+        .then(function (response) {
+            return response.json();
+        });
+        console.log("quote: ", response);
+        displayQuote();
+}
+
+function fetchGiphy(wordValue) {
+    var giphyURL = `api.giphy.com/v1/gifs/search?q=${wordValue}&api_key=T5jopP1Bh8SGzs6g1b6MrMdb26IrnDeC`;
+            fetch(giphyURL)
+                .then(function (response) {
+                    return response.json();
+                });
+                console.log("giphy: ", response)
+                displayGiphy();
+}
+
+function displayQuote() {
+
+}
+
+function displayGiphy() {
+
+}
+
+function displaySavedWords() {
+    if (localStorage.getItem("word")) {
+        loggedWords = JSON.parse(localStorage.getItem("word"));
+    }
+
+    var wordList = "";
+    for (let i = 0; i < loggedWords.length; i++) {
+        wordList = wordList + `<button class="my-2" type="submit">${loggedWords[i]}`;
+    }
+
+    savedWords.innerHTML - wordList;
+
+    var buttonMargin = document.querySelectorAll(".my-2");
+    for (let i = 0; i < buttonMargin.length; i++) {
+        buttonMargin[i].addEventListener("click", function() {
+            displayQuote(this.textContent);
+            displayGiphy(this.textContent);
+        });
+    }
+}
+
+
+
+// function displayResults(wordValue) {
+//     var quoteURL = `https://zenquotes.io/api/random?option1=${wordValue}`;
+//     fetch(quoteURL)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (giphyData) {
+//             var giphyURL = `api.giphy.com/v1/gifs/search?q=${wordValue}&api_key=T5jopP1Bh8SGzs6g1b6MrMdb26IrnDeC`;
+//             fetch(giphyURL)
+//                 .then(function (response) {
+//                     return response.json();
+//                 })
+//                 .then (function ())
+//         })
+// }
+
+
+wordInputForm.addEventListener("submit", submitWord);
