b1a5889e4f9037b9a203cadec6a1d670b0084950
stevensillsii <stevensills2@gmail.com>
2023-10-12 13:48:09 -0700

    Granim is now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,3 +108,20 @@ function clearSavedWords() {
 clearWordsButton.addEventListener("click", function() {
     clearSavedWords();
 })
+
+
+// this is our Granim stuff
+var granimInstance = new Granim({
+    element: '#canvas-basic',
+    direction: 'left-right',
+    isPausedWhenNotInView: true,
+    states : {
+        "default-state": {
+            gradients: [
+                ['#ff9966', '#ff5e62'],
+                ['#00F260', '#0575E6'],
+                ['#e1eec3', '#f05053']
+            ]
+        }
+    }
+});
